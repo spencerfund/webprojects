ddc5fc86f334891248a9c91d62bfc65a0e07897e
Spencer Fund <fund.spencer@gmail.com>
2023-05-10 10:22:46 -0600

    finished everything but logic for win

--- Training/Project 4/script.js
+++ Training/Project 4/script.js
@@ -7,9 +7,11 @@ function nextTurn() {
         if (xTurn) {
             document.querySelectorAll('.Piece_O').forEach(o => {
                 o.setAttribute('draggable', false);
+                o.style.cursor = 'default';
             });
             document.querySelectorAll('.Piece_X').forEach(x => {
                 x.setAttribute('draggable', true);
+                x.style.cursor = 'grab';
             });
             let turnIndicator = document.getElementById('turnIndicator');
             turnIndicator.innerHTML = "X Turn";
@@ -17,14 +19,21 @@ function nextTurn() {
         } else {
             document.querySelectorAll('.Piece_O').forEach(o => {
                 o.setAttribute('draggable', true);
+                o.style.cursor = 'grab';
             });
             document.querySelectorAll('.Piece_X').forEach(x => {
                 x.setAttribute('draggable', false);
+                x.style.cursor = 'default';
             });
             let turnIndicator = document.getElementById('turnIndicator');
             turnIndicator.innerHTML = "O Turn";
             turnIndicator.style.color = "blue";
         }
+        const gameBoard = document.querySelector('#game_board');
+        gameBoard.querySelectorAll('.Game_Piece').forEach(piece => {
+            piece.style.cursor = 'default';
+            piece.setAttribute('draggable', false);
+        })
     } else {
         if (xWin) {
             window.alert(["X Wins!"]);
@@ -35,7 +44,12 @@ function nextTurn() {
 };
 
 function checkWin() {
-    gameOver = true;
+    const gameBoard = document.querySelector('#game_board');
+    gameBoard.querySelectorAll('tr').forEach(row => {
+        row.querySelectorAll('td').forEach(cell => {
+
+        });
+    });
 }
 
 const drag = event => {
@@ -71,10 +85,13 @@ document.querySelectorAll('.Game_Piece').forEach(piece => {
 
 const drop = event => {
     document.querySelectorAll('.box').forEach(box => box.classList.remove('Drop_Piece'));
+    document.querySelectorAll('.box').forEach(box => box.classList.remove('Game_Piece'));
     let item = document.querySelector(`[data-id="${event.dataTransfer.getData('text/plain')}"]`);
     item.remove();
 
     event.currentTarget.innerHTML = event.currentTarget.innerHTML + event.dataTransfer.getData('text/html');
+    event.currentTarget.removeAttribute('ondrop');
+    event.currentTarget.removeAttribute('ondragover');
     item = document.querySelector(`[data-id="${event.dataTransfer.getData('text/plain')}"]`);
     item.removeAttribute('draggable');
     
